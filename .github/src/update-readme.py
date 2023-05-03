854309cb058fc6ab8f1a7c8f5110bc3209428ba4
Yuseop Lee <90027671+lys7aves@users.noreply.github.com>
2023-05-03 18:15:50 +0900

    Update update-readme.py

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -25,7 +25,7 @@ print(patterns)
 # ignore 패턴과 일치하는지 확인하는 함수
 def check_ignore_pattern(item_path):
     for pattern in patterns:
-        if re.fullmatch(pattern, item_path[(len(dir_path)+1):]):  # 항상 붙는 "[dir_path]/" 제거
+        if re.fullmatch(pattern, item_path[2:]):  # 항상 붙는 "[dir_path]/" 제거
             return True
     return False
 
